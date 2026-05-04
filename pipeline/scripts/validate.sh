aa26b6f7d2809aef1b8b6cff012dce9eb89be823
katsura(delphinus2) <yasuhiro.katsura0522@gmail.com>
2026-05-04 12:27:33 +0900

    TARGET_LAYERの修正

--- pipeline/scripts/validate.sh
+++ pipeline/scripts/validate.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+set -e # エラー発生時にスクリプトを終了
+
 TARGET_LAYER="$1"
 
-echo "Processing directory: $TARGET_LAYER"
+echo "Validating Terraform configuration for $TARGET_LAYER."
 
 terraform init -backend=false # HCPに接続せずローカルで構文チェックのみ行う
 terraform validate
+
+echo "Task completed successfully!"
