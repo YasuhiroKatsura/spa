18e749b85333347634f8d3085d8e34f954567ff1
katsura(delphinus2) <yasuhiro.katsura0522@gmail.com>
2026-05-04 20:34:50 +0900

    実行環境, Pipine起動条件を修正

--- terraform/03_landing_zone/env/dev/01_main.tf
+++ terraform/03_landing_zone/env/dev/01_main.tf
@@ -1,11 +1,19 @@
+# -----実行環境-----
+terraform {
+  cloud {
+    organization = "okamura"
+    workspaces {
+      name = "03_landing_zone"
+    }
+  }
+}
+
 # -----変数定義-----
 variable "common" {
   type = map(string)
   default = {
     project_name = "spa"
     region  = "ap-northeast-1"
-    tfc_organization = "okamura"
-    tfc_workspace = "03_landing_zone"
   }
 }
 
@@ -31,16 +39,6 @@ variable "private_subnets" {
   default = {
     # "a" = "10.0.2.0/24"
     # "c" = "10.0.3.0/24"
-  }
-}
-
-# -----実行環境-----
-terraform {
-  cloud {
-    organization = var.common.tfc_organization
-    workspaces {
-      name = var.common.tfc_workspace
-    }
   }
 }
 
