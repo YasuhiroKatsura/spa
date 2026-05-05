abfba1d3a20afd41e7fbc771eebf30450c5748a2
katsura(delphinus2) <yasuhiro.katsura0522@gmail.com>
2026-05-05 10:02:57 +0900

    echoの表示色を変更

--- pipeline/scripts/feature_plan.sh
+++ pipeline/scripts/feature_plan.sh
@@ -5,18 +5,18 @@ set -e # エラー発生時にスクリプトを終了
 AWS_ENV="$1"
 TARGET_LAYER="$2"
 
-echo "Changing directory to:" $TARGET_LAYER
+echo -e "\e[34mChanging directory to:" $TARGET_LAYER "\e[0m"
 cd terraform/$TARGET_LAYER/env/$AWS_ENV
 pwd; ls -a
 
-echo "Initializing Terraform."
+echo -e "\e[34mInitializing Terraform.\e[0m"
 terraform init
 
-echo "Validating Terraform configuration."
+echo -e "\e[34mValidating Terraform configuration.\e[0m"
 terraform validate
 
-echo "Planning Terraform changes."
+echo -e "\e[34mPlanning Terraform changes.\e[0m"
 # terraform plan -out=tfplan # バイナリに吐き出したほうがいいとは思うがどこに吐き出すか設計してない
 terraform plan
 
-echo "Task completed successfully!"
+echo -e "\e[32mTask completed successfully!\e[0m"
