ef37a1d2081adcfd78c933f366e435af5e2d0e42
katsura(delphinus2) <yasuhiro.katsura0522@gmail.com>
2026-05-16 11:14:35 +0900

    bugfix

--- terraform/01_core_infra/env/dev/02_spa_api.tf
+++ terraform/01_core_infra/env/dev/02_spa_api.tf
@@ -96,8 +96,8 @@ resource "aws_ecs_task_definition" "ecs_task4api" {
       db_host = aws_rds_cluster.aurora_cluster.endpoint
       db_port = aws_rds_cluster.aurora_cluster.port
       db_name = aws_rds_cluster.aurora_cluster.database_name
-      db_user = "${var.spa_db.aurora_username}"
-      db_password = "${var.spa_db.aurora_password}"
+      db_user = aws_rds_cluster.aurora_cluster.master_username
+      db_secret_arn = aws_rds_cluster.aurora_cluster.master_user_secret[0].secret_arn
     }
   )
 
